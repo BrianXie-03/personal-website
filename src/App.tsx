99dafc6a6425fa3a86b79d040da75181375c8dcf
Skyen09 <brianxie1298@gmail.com>
2024-06-07 23:00:17 -0400

    dsadas

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import Navbar from "@/scenes/navbar";
+
+function App() {
+  return( 
+    <div className="app bg-blue-100">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
+// import Logo from "@/assets/Logo.png"
+type Props = {};
+
+const Navbar = (props: Props) => {
+    const flexBetween = "flex item-center justify-between"
+    return <div>Poopie</div>;
+}
+
+export default Navbar;
